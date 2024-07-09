71380e3b3bc09c6376ef9b2be318c9b95dc4225e
ThiagoG1809 <thiagogimenezzz@hotmail.com>
2024-07-08 23:06:32 -0300

    actualizacion n1

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/EWIEGA46WW/moc_mainwindow.cpp.d
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/EWIEGA46WW/moc_mainwindow.cpp.d
@@ -1,5 +1,7 @@
-C:/Users/Alumno/Documents/PaperPlease/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/EWIEGA46WW/moc_mainwindow.cpp: C:/Users/Alumno/Documents/PaperPlease/mainwindow.h \
-  C:/Users/Alumno/Documents/PaperPlease/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/moc_predefs.h \
+C:/Users/thiag/OneDrive/Escritorio/TRABAJO\ PRACTICO\ POO\ ANASHE/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/EWIEGA46WW/moc_mainwindow.cpp: C:/Users/thiag/OneDrive/Escritorio/TRABAJO\ PRACTICO\ POO\ ANASHE/Paper-Please/mainwindow.h \
+  C:/Users/thiag/OneDrive/Escritorio/TRABAJO\ PRACTICO\ POO\ ANASHE/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/moc_predefs.h \
+  C:/Qt/6.7.2/mingw_64/include/QtCore/QString \
+  C:/Qt/6.7.2/mingw_64/include/QtCore/QStringList \
   C:/Qt/6.7.2/mingw_64/include/QtCore/q20functional.h \
   C:/Qt/6.7.2/mingw_64/include/QtCore/q20memory.h \
   C:/Qt/6.7.2/mingw_64/include/QtCore/q20type_traits.h \
@@ -348,4 +350,5 @@ C:/Users/Alumno/Documents/PaperPlease/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/
   C:/Qt/Tools/mingw1120_64/x86_64-w64-mingw32/include/sys/types.h \
   C:/Qt/Tools/mingw1120_64/x86_64-w64-mingw32/include/time.h \
   C:/Qt/Tools/mingw1120_64/x86_64-w64-mingw32/include/vadefs.h \
-  C:/Qt/Tools/mingw1120_64/x86_64-w64-mingw32/include/wchar.h
+  C:/Qt/Tools/mingw1120_64/x86_64-w64-mingw32/include/wchar.h \
+  C:/Users/thiag/OneDrive/Escritorio/TRABAJO\ PRACTICO\ POO\ ANASHE/Paper-Please/persona.h
