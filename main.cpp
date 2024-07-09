e057a4e03858e195ed904464cfdf9adc02daacbe
facurz55 <facurz55@gmail.com>
2024-07-09 11:25:21 -0300

    Menu Nuevo

--- main.cpp
+++ main.cpp
@@ -1,5 +1,4 @@
 #include "mainwindow.h"
-#include "persona.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
@@ -7,9 +6,6 @@ int main(int argc, char *argv[])
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    Persona Persona1;
-    Persona1.Mostrar_Datos();
-    w.actualizarLabelConPersona(Persona1);
 
     return a.exec();
 }
