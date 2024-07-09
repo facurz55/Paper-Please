63f035e4e6337575cc6f0ce4cba72e353846163e
Mariano-G30 <marianogervasoni25@gmail.com>
2024-07-08 23:17:33 -0300

    ño

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/include/ui_mainwindow.h
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease_autogen/include/ui_mainwindow.h
@@ -11,9 +11,8 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
 #include <QtWidgets/QMainWindow>
-#include <QtWidgets/QMenuBar>
-#include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
 QT_BEGIN_NAMESPACE
@@ -22,8 +21,7 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
-    QMenuBar *menubar;
-    QStatusBar *statusbar;
+    QLabel *label;
 
     void setupUi(QMainWindow *MainWindow)
     {
@@ -32,14 +30,15 @@ public:
         MainWindow->resize(800, 600);
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
+        centralwidget->setStyleSheet(QString::fromUtf8("background-color: black;"));
+        label = new QLabel(centralwidget);
+        label->setObjectName("label");
+        label->setGeometry(QRect(180, 100, 411, 261));
+        label->setStyleSheet(QString::fromUtf8("#label{\n"
+"background-color: green;\n"
+"color: white;\n"
+"}"));
         MainWindow->setCentralWidget(centralwidget);
-        menubar = new QMenuBar(MainWindow);
-        menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 21));
-        MainWindow->setMenuBar(menubar);
-        statusbar = new QStatusBar(MainWindow);
-        statusbar->setObjectName("statusbar");
-        MainWindow->setStatusBar(statusbar);
 
         retranslateUi(MainWindow);
 
@@ -49,6 +48,7 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        label->setText(QString());
     } // retranslateUi
 
 };
