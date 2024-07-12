568f2558d1b699a2231890b17e7e9ac6406afcb0
Mariano-G30 <marianogervasoni25@gmail.com>
2024-07-12 18:29:10 -0300

    123

--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
@@ -1,3 +1,3 @@
-set(__QT_DEPLOY_TARGET_PaperPlease_FILE C:/Users/thiag/OneDrive/Escritorio/REPOSITORIO 3/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease.exe)
+set(__QT_DEPLOY_TARGET_PaperPlease_FILE C:/Users/maria/OneDrive/Documentos/GitHub/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease.exe)
 set(__QT_DEPLOY_TARGET_PaperPlease_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_PaperPlease_RUNTIME_DLLS C:/Qt/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.7.2/mingw_64/bin/Qt6Core.dll)
+set(__QT_DEPLOY_TARGET_PaperPlease_RUNTIME_DLLS C:/Users/maria/OneDrive/Desktop/Nuevacarpeta/6.7.2/mingw_64/bin/Qt6Widgets.dll;C:/Users/maria/OneDrive/Desktop/Nuevacarpeta/6.7.2/mingw_64/bin/Qt6Gui.dll;C:/Users/maria/OneDrive/Desktop/Nuevacarpeta/6.7.2/mingw_64/bin/Qt6Core.dll)
--- build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/cmake_install.cmake
+++ build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/cmake_install.cmake
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 # Install script for directory: C:/Users/maria/OneDrive/Documentos/GitHub/Paper-Please
-=======
-# Install script for directory: C:/Users/thiag/OneDrive/Escritorio/REPOSITORIO 3/Paper-Please
->>>>>>> main
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,11 +38,7 @@ if(NOT DEFINED CMAKE_OBJDUMP)
 endif()
 
 if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-<<<<<<< HEAD
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/maria/OneDrive/Documentos/GitHub/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease.exe")
-=======
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "C:/Users/thiag/OneDrive/Escritorio/REPOSITORIO 3/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/PaperPlease.exe")
->>>>>>> main
   if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/PaperPlease.exe" AND
      NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/PaperPlease.exe")
     if(CMAKE_INSTALL_DO_STRIP)
@@ -63,9 +55,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-<<<<<<< HEAD
 file(WRITE "C:/Users/maria/OneDrive/Documentos/GitHub/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
-=======
-file(WRITE "C:/Users/thiag/OneDrive/Escritorio/REPOSITORIO 3/Paper-Please/build/Desktop_Qt_6_7_2_MinGW_64_bit-Debug/${CMAKE_INSTALL_MANIFEST}"
->>>>>>> main
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
