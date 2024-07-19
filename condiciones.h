683f7b4c13587bdb76adcb03f126fcb60622d820
facurz55 <facurz55@gmail.com>
2024-07-18 21:24:05 -0300

    CondicionesNivel1

---
+++ condiciones.h
@@ -0,0 +1,39 @@
+#ifndef CONDICIONES_H
+#define CONDICIONES_H
+#include <QString>
+#include "persona.h"
+
+class condiciones
+{
+public:
+    condiciones();
+    ~condiciones(); //Para limpiar la memoria
+
+    void generarCondicionesNivel1();
+    int generarNumero();
+    int generarNumero2();
+
+    QString obtenerNacionalidad();
+    QString obtenerFecha();
+    QString obtenerTipoVisita();
+    QString obtenerEstancia();
+    QString obtenerEstCivil();
+
+protected:
+    QString nacionalidad;
+    QString fecha;
+    QString tipoVisita;
+    QString estancia;
+    QString estCivil;
+    QString paisResidencia;
+    QString proposito;
+    QString compania;
+    QString objetos;
+    QString tipoCarreraUni;
+
+private:
+    Persona* NPC;
+
+};
+
+#endif // CONDICIONES_H
